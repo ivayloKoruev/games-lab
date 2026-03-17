dfe7fe0d92d7e35bf23ad8c38172deec3cf85847
Ivaylo Koruev <ivaylokoruev98@gmail.com>
2026-03-17 10:43:42 +0200

    Add settings button functionality and UI updates

--- Sweet Donuts/game.js
+++ Sweet Donuts/game.js
@@ -9,7 +9,8 @@ let candyDragged = null;
 let gameInterval = null;
 let targetScore = 2000;
 
-
+const okButton = document.querySelector(".okButton");
+const settingsInfo = document.querySelector(".settingsInfo");
 const title = document.querySelector(".title");
 const buttonsClass = document.querySelector(".buttons");
 const boardElement = document.getElementById("board");
@@ -21,6 +22,7 @@ const scoreDivElement = document.getElementById("scoreDiv");
 document.getElementById("target").innerHTML = targetScore;
 let scoreScale = document.querySelector(".scoreScale");
 document.addEventListener("DOMContentLoaded", function () {
+  const settingsButton = document.getElementById("settingsButton");
   const buttonAudio = document.getElementById("buttonAudio");
   const bgAudio = document.getElementById("audio");
   const textInfo = document.querySelector(".textInfo");
@@ -43,21 +45,41 @@ document.addEventListener("DOMContentLoaded", function () {
     scoreDivElement.style.opacity = "1";
   });
 
-  infoButton.addEventListener("click", function () {
-    startButton.style.display = "none";
-    infoButton.style.display = "none";
-    textInfo.classList.remove("hide");
-    textInfo.style.opacity = "1";
+  okButton.addEventListener("click", function() {
     buttonAudio.play();
-    title.classList.add("hide");
-  });
-
-  exitButton.addEventListener("click", function () {
-    buttonAudio.play();
-    textInfo.classList.add("hide");
+    settingsInfo.classList.remove("show");
+    settingsButton.style.display = "flex";
     startButton.style.display = "flex";
     infoButton.style.display = "flex";
     title.classList.remove("hide");
+  });
+  
+  infoButton.addEventListener("click", function () {
+    startButton.style.display = "none";
+    infoButton.style.display = "none";
+    textInfo.classList.add("show");
+    buttonAudio.play();
+    title.classList.add("hide");
+    settingsButton.style.display = "none";
+  });
+
+  settingsButton.addEventListener("click", function() {
+    buttonAudio.play();
+    settingsButton.style.display = "none";
+    startButton.style.display = "none";
+    infoButton.style.display = "none";
+    title.classList.add("hide");
+    settingsInfo.classList.add("show");
+  });
+
+
+  exitButton.addEventListener("click", function () {
+    buttonAudio.play();
+    textInfo.classList.remove("show");
+    startButton.style.display = "flex";
+    infoButton.style.display = "flex";
+    title.classList.remove("hide");
+    settingsButton.style.display = "flex";
   });
 });
 
@@ -68,8 +90,11 @@ window.onload = function () {
 
 function processMatches() {
   if (checkMatch()) {
+    // ако има съвпадения, първо премахваме, след това падаме и генерираме нови бонбони
     slideCandy();
     generateCandy();
+
+    // извикваме отново след 200ms, за да браузърът успее да обнови DOM
     setTimeout(processMatches, 200);
   }
 }
@@ -98,17 +123,23 @@ function startGame() {
 
       candy.addEventListener("drop", (e) => {
         let droppedCandy = e.target;
+
+        //проверяваме за съседни елеметни откъм 4те страни - ляво, дясно, горе и долу
+        //взимаме id на колоните и редовете на елемента който придвижваме
         let draggedCoords = candyDragged.id.split("-");
         let draggedRow = parseInt(draggedCoords[0]);
         let draggedCol = parseInt(draggedCoords[1]);
 
+        //взимаме id на колоните и редовете на съседния елемент
         let droppedCoords = droppedCandy.id.split("-");
         let droppedRow = parseInt(droppedCoords[0]);
         let droppedCol = parseInt(droppedCoords[1]);
 
+        //взима дистанцията (id-то на елемента който придвижваме минус съседния му)
         let rowDiff = Math.abs(draggedRow - droppedRow);
         let colDiff = Math.abs(draggedCol - droppedCol);
-        
+
+        //проверяваме ако дистанцията е 0 или 1 но не повече, тогава може да ги размениш
         if (
           (rowDiff === 1 && colDiff === 0) ||
           (rowDiff === 0 && colDiff === 1)
@@ -150,7 +181,7 @@ function crushCandy() {
 
 function checkMatch() {
   let matchFound = false;
-  
+  //проверяваме редовете
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < cols - 2; c++) {
       let candy1 = board[r][c];
@@ -177,6 +208,7 @@ function checkMatch() {
     }
   }
 
+  //проверяваме колоните
   for (let c = 0; c < cols; c++) {
     for (let r = 0; r < rows - 2; r++) {
       let candy1 = board[r][c];
@@ -244,5 +276,4 @@ function checkScore() {
         }, 1000);
       }, 800);
     }
-
 }
